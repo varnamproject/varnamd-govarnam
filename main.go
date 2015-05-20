3fe2eee7b09b2e9351c4f397ba26c9302dfc5daf
Navaneeth K N <navaneethkn@gmail.com>
2015-05-20 12:22:13 +0530

    Added load and save config methods

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "flag"
-import "fmt"
-import "os"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"runtime"
+)
 
 var (
 	port           int
@@ -12,7 +18,80 @@ var (
 	learnPort      int
 	host           string
 	uiDir          string
+	varnamdConfig  *config // config instance used across the applicarion
 )
+
+// varnamd configurations
+// usually resides in $HOME/.varnamd/config on POSIX and APPDATA/.varnamd/config on Windows
+type config struct {
+	Upstream      string   `json:"upstream"`
+	SchemesToSync []string `json:"schemesToSync"`
+}
+
+func initDefaultConfig() *config {
+	return &config{Upstream: "http://api.varnamproject.com", SchemesToSync: []string{}}
+}
+
+func getConfigFilePath() string {
+	var configDir string
+	if runtime.GOOS == "windows" {
+		configDir = path.Join(os.Getenv("localappdata"), ".varnamd")
+	} else {
+		configDir = path.Join(os.Getenv("HOME"), ".varnamd")
+	}
+
+	configFilePath := path.Join(configDir, "config.json")
+	return configFilePath
+}
+
+func loadConfigFromFile() *config {
+	configFilePath := getConfigFilePath()
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		return initDefaultConfig()
+	}
+	defer configFile.Close()
+
+	jsonDecoder := json.NewDecoder(configFile)
+	var c config
+	err = jsonDecoder.Decode(&c)
+	if err != nil {
+		log.Printf("%s is malformed. Using default config instead\n", configFilePath)
+		return initDefaultConfig()
+	}
+
+	return &c
+}
+
+func saveConfigToFile() error {
+	if varnamdConfig == nil {
+		panic("config is not initialized")
+	}
+
+	configFilePath := getConfigFilePath()
+	err := os.MkdirAll(path.Dir(configFilePath), 0777)
+	if err != nil {
+		return err
+	}
+
+	configFile, err := os.Create(configFilePath)
+	if err != nil {
+		return err
+	}
+	defer configFile.Close()
+
+	b, err := json.MarshalIndent(varnamdConfig, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	_, err = configFile.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func init() {
 	flag.IntVar(&port, "p", 8080, "Run daemon in specified port")
@@ -22,6 +101,7 @@ func init() {
 	flag.StringVar(&host, "host", "", "Host for the varnam daemon server")
 	flag.StringVar(&uiDir, "ui", "", "UI directory path")
 	flag.BoolVar(&version, "version", false, "Print the version and exit")
+	varnamdConfig = loadConfigFromFile()
 }
 
 func main() {
