a68eff6bbc36f79bb54a517bfc5000f83b6ac73b
Subin Siby <mail@subinsb.com>
2022-01-29 15:21:55 +0530

    Refactored Caching (#2)
    * Refactored caching mechanism, start caching /atl endpoints
    * Increase cache limit

--- cache.go
+++ cache.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/gob"
 	"strings"
 	"time"
 
@@ -9,65 +10,90 @@ import (
 )
 
 const (
-	defaultCacheSize = 100 << 20      // 100 MB cache
-	defaultExpiry    = time.Hour * 24 // Cache for 24 hours
-	wordSeparator    = "<>"           // Assuming the separator wont be used in any case.
+	defaultCacheSize = 1000 << 20      // 1000 MB cache
+	defaultExpiry    = time.Hour * 120 // Cache for 120 hours
+	stringSeparator  = "<>"            // Assuming the separator wont be used in any case.
 )
 
 // Cache objects.
-type Cache interface {
-	Set(lang, word string, val ...string) error
-	Get(lang, word string) ([]string, error)
-	Delete(lang, word string) (bool, error)
-	Clear()
-}
-
-// MemCache impliments Cache interface.
-type MemCache struct {
+type Cache struct {
 	fc *freecache.Cache
 }
 
-// NewMemCache will create object of new Cache impliemtation.
-func NewMemCache() *MemCache {
-	return newCacheWithSize(defaultCacheSize)
+type GeneralCacheItem struct {
+	Value interface{}
 }
 
-func newCacheWithSize(size int) *MemCache {
-	return &MemCache{fc: freecache.NewCache(size)}
+// NewCache will create a new freecache
+func NewCache() Cache {
+	return Cache{fc: freecache.NewCache(defaultCacheSize)}
 }
 
-// Set lang-word as val to cache.
-func (c *MemCache) Set(lang, word string, val ...string) error {
-	var value = strings.Join(val, wordSeparator)
-	return c.setWithExpiry(lang, word, []byte(value), int(defaultExpiry))
+// Set a string value to cache.
+func (c *Cache) SetString(key string, val ...string) error {
+	var value = strings.Join(val, stringSeparator)
+	return c.setWithExpiry(key, []byte(value), int(defaultExpiry))
 }
 
-func (c *MemCache) setWithExpiry(lang, word string, val []byte, expiry int) error {
-	var key = fmt.Sprintf("%s-%s", lang, word)
+func (c *Cache) setWithExpiry(key string, val []byte, expiry int) error {
 	return c.fc.Set([]byte(key), val, expiry)
 }
 
-// Get lang-word from cache.
-func (c *MemCache) Get(lang, word string) ([]string, error) {
-	var key = fmt.Sprintf("%s-%s", lang, word)
-
+// Get a string value from cache.
+func (c *Cache) GetString(key string) ([]string, error) {
 	val, err := c.fc.Get([]byte(key))
 	if err != nil {
 		return nil, err
 	}
 
-	return strings.Split(string(val), wordSeparator), nil
+	return strings.Split(string(val), stringSeparator), nil
 }
 
-// Delete lang-word from cache.
-func (c *MemCache) Delete(lang, word string) (bool, error) {
-	var key = fmt.Sprintf("%s-%s", lang, word)
-	affected := c.fc.Del([]byte(key))
+// Set a generic cache value.
+// These are about 100 micro-seconds (3 times) slower
+// than the string cache above. It's slower because of
+// encoding and decoding
+func (c *Cache) Set(key string, value interface{}) error {
+	var valueBytesBuffer bytes.Buffer
 
+	// gob can only encode struct items.
+	// It can't encode arrays properly
+	cacheItem := GeneralCacheItem{value}
+
+	enc := gob.NewEncoder(&valueBytesBuffer)
+	err := enc.Encode(cacheItem)
+	if err != nil {
+		return err
+	}
+
+	return c.fc.Set([]byte(key), valueBytesBuffer.Bytes(), int(defaultExpiry))
+}
+
+// Get a value from cache.
+func (c *Cache) Get(key string) (interface{}, error) {
+	valueBytes, err := c.fc.Get([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	var cacheItem GeneralCacheItem
+
+	dec := gob.NewDecoder(bytes.NewReader(valueBytes))
+	err = dec.Decode(&cacheItem)
+	if err != nil {
+		return nil, err
+	}
+
+	return cacheItem.Value, nil
+}
+
+// Delete a value from cache.
+func (c *Cache) Delete(key string) (bool, error) {
+	affected := c.fc.Del([]byte(key))
 	return affected, nil
 }
 
 // Clear everything in the cache.
-func (c *MemCache) Clear() {
+func (c *Cache) Clear() {
 	c.fc.Clear()
 }
